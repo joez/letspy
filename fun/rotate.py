1deecd3c1e15cab7bce71df2848cce1dabc00a50
Joe Zheng <joe.zheng.cn@gmail.com>
2019-07-02 23:21:26 +0800

    rotate the array to the right by k steps

---
+++ fun/rotate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+def rotate(nums, k):
+    '''
+    rotate the array to the right by k steps, k is non-negative
+
+    >>> rotate([1, 2, 3, 4, 5, 6], 3)
+    [4, 5, 6, 1, 2, 3]
+    '''
+    n = len(nums)
+    k = k % n
+    for i, j in [(0, n-k-1), (n-k, n-1), (0, n-1)]:
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i, j = i+1, j-1
+
+
+if __name__ == "__main__":
+    k = int(input())
+    nums = [int(n) for n in input().split()]
+    rotate(nums, k)
+    print(nums)
